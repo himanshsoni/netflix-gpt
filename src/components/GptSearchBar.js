37f680d80c860d181bc90a5f8d608c7aa5542586
himanshsoni <himanshsoni5@gmail.com>
2024-02-11 19:26:20 +0530

    handle error scenario for open-api

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { openai } from "../utils/openAI";
 import { API_OPTIONS } from "../utils/constant";
 import { useDispatch } from "react-redux";
@@ -7,6 +7,7 @@ import { addGptMoviesResult } from "../utils/gptSlice";
 const GptSearchBar = () => {
   const searchTxt = useRef(null);
   const dispatch = useDispatch();
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const fetchMoviesTMDB = async (movie) => {
     const data = await fetch(
@@ -18,48 +19,58 @@ const GptSearchBar = () => {
   };
 
   const handleGptSearch = async () => {
+    console.log("handleGptSearch");
     const gptQuery =
       "Act as a Movie Recommendation system and suggest some movies for the query : " +
       searchTxt.current.value +
       ". only give me names of 5 movies, comma seperated like the example result given ahead. Example Result: Dhoom, Sholay, Don,Krish,Welcome";
 
-    const data = await openai.chat.completions.create({
-      messages: [{ role: "user", content: gptQuery }],
-      model: "gpt-3.5-turbo",
-    });
+    try {
+      const data = await openai.chat.completions.create({
+        messages: [{ role: "user", content: gptQuery }],
+        model: "gpt-3.5-turbo",
+      });
+      const gptMovies = (data?.choices?.[0]?.message.content).split(",");
 
-    if (!data.choices) {
+      const promiseArray = gptMovies.map((movie) => fetchMoviesTMDB(movie));
+
+      const moviesData = await Promise.all(promiseArray);
+      dispatch(addGptMoviesResult(moviesData));
+    } catch (error) {
+      setErrorMessage(error.message);
     }
-
-    const gptMovies = (data.choices?.[0]?.message.content).split(",");
-
-    const promiseArray = gptMovies.map((movie) => fetchMoviesTMDB(movie));
-
-    const moviesData = await Promise.all(promiseArray);
-    dispatch(addGptMoviesResult(moviesData));
   };
 
   return (
-    <div className="pt-[50%]  md:pt-[10%] flex justify-center">
-      <form
-        action=""
-        className="w-full  md:w-1/2 bg-black grid grid-cols-12 rounded-lg"
-        onSubmit={(e) => e.preventDefault()}
-      >
-        <input
-          ref={searchTxt}
-          className="p-4 m-4 col-span-9"
-          type="text"
-          placeholder="What movie you would like to see?"
-        />
-        <button
-          className="col-span-3 m-4 py-2 px-4 bg-red-500 rounded-lg text-white"
-          onClick={handleGptSearch}
+    <>
+      <div className="pt-[50%]  md:pt-[10%] flex justify-center">
+        <form
+          action=""
+          className="w-full  md:w-1/2 bg-black grid grid-cols-12 rounded-lg"
+          onSubmit={(e) => e.preventDefault()}
         >
-          Search
-        </button>
-      </form>
-    </div>
+          <input
+            ref={searchTxt}
+            className="p-4 m-4 col-span-9"
+            type="text"
+            placeholder="What movie you would like to see?"
+          />
+          <button
+            className="col-span-3 m-4 py-2 px-4 bg-red-500 rounded-lg text-white"
+            onClick={handleGptSearch}
+          >
+            Search
+          </button>
+        </form>
+      </div>
+      <div>
+        {errorMessage && (
+          <>
+            <p className="text-white text-3xl"> {errorMessage}</p>
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
